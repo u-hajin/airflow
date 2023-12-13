a4eefb832fe827805dc451494d67e1677d796fa1
YooSuYeon <usuyn000@gmail.com>
2023-12-14 00:06:11 +0900

    커넥션 테스트용 dag 업로드

---
+++ dags/dags_conn_test.py
@@ -0,0 +1,48 @@
+import datetime
+import pendulum
+
+from airflow import DAG
+from airflow.operators.bash import EmptyOperator # 아무것도 하지 않는 operator
+
+with DAG(
+    dag_id="dags_bash_operator",
+    schedule=None,
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    
+    t1 = EmptyOperator(
+        task_id="t1"
+    )
+
+    t2 = EmptyOperator(
+        task_id="t2"
+    )
+
+    t3 = EmptyOperator(
+        task_id="t3"
+    )
+
+    t4 = EmptyOperator(
+        task_id="t4"
+    )
+
+    t5 = EmptyOperator(
+        task_id="t5"
+    )
+
+    t6 = EmptyOperator(
+        task_id="t6"
+    )
+
+    t7 = EmptyOperator(
+        task_id="t7"
+    )
+
+    t8 = EmptyOperator(
+        task_id="t8"
+    )
+
+    t1 >> [t2, t3] >> t4
+    t5 >> t4
+    [t4, t7] >> t6 >> t8
