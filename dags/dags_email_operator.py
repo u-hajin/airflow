9ac487e1aa65b58e0f54d248e7fd0933dd1af547
YooSuYeon <usuyn000@gmail.com>
2023-12-14 18:58:03 +0900

    email operator 업로드

---
+++ dags/dags_email_operator.py
@@ -0,0 +1,18 @@
+import pendulum
+import datetime
+
+from airflow import DAG
+from airflow.operators.email import EmailOperator
+
+with DAG(
+    dag_id="dags_email_operator",
+    schedule="0 8 1 * *", # 매월 1일 아침 8시 
+    start_date=pendulum.datetime(2023, 12, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    send_email_task = EmailOperator(
+        task_id="send_email_task",
+        to="usuyn027@gmail.com"
+        subject="Airflow 성공 메일"
+        html_content="Airflow 작업이 완료되었습니다."
+    )
