d22cd7238ffc8bf7d1b73c1bd2324a0599ebc8d7
YooSuYeon <usuyn000@gmail.com>
2023-12-18 00:45:49 +0900

    bash macro 업로드

--- dags/dags_bash_with_macro_eg1.py
+++ dags/dags_bash_with_macro_eg1.py
@@ -14,7 +14,7 @@ with DAG(
         task_id="bash_task_1",
         env={
             "START_DATE":"{{ data_interval_start.in_timezone('Asia/Seoul') | ds }}", # 템플릿 변수들은 기본적으로 UTC tz
-            "END_DATE":"{{ (data_inerval_end.in_timezone('Asia/Seoul') - macros.dateutil.relativedelta.relativedelta(days=1)) | ds }}" # 연산자가 -라 알아서 1일 빼줌
+            "END_DATE":"{{ (data_interval_end.in_timezone('Asia/Seoul') - macros.dateutil.relativedelta.relativedelta(days=1)) | ds }}" # 연산자가 -라 알아서 1일 빼줌
         },
         bash_command="echo 'START_DATE: $START_DATE' && echo 'END_DATE: $END_DATE'"
     )
