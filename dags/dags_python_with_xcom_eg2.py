13118a5aa5ae59896388b06bb7e8fa4a9887eb54
YooSuYeon <usuyn000@gmail.com>
2023-12-18 21:31:23 +0900

    task간 데이터 공유 xcom 업로드

--- dags/dags_python_with_xcom_eg2.py
+++ dags/dags_python_with_xcom_eg2.py
@@ -4,7 +4,7 @@ from airflow import DAG
 from airflow.decorators import task
 
 with DAG(
-    dag_id="dags_python_with_xcom_eg1",
+    dag_id="dags_python_with_xcom_eg2",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
