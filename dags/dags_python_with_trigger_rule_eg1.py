4f39a666de5561dc6bcfa367115a25e769a15093
YooSuYeon <usuyn000@gmail.com>
2023-12-22 01:09:33 +0900

    trigger rule 설정

---
+++ dags/dags_python_with_trigger_rule_eg1.py
@@ -0,0 +1,34 @@
+import pendulum
+
+from airflow import DAG
+from airflow.decorators import task
+from airflow.exceptions import AirflowException
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id='dags_python_with_trigger_rule_eg1',
+    start_date=pendulum.datetime(2023, 12, 1, tz='Asia/Seoul'),
+    schedule=None,
+    catchup=False
+) as dag:
+    bash_upstream_1 = BashOperator(
+        task_id='bash_upstream_1',
+        bash_command='echo upstream1'
+    )
+    
+    @task(task_id='python_upstream_1')
+    def python_upstream_1():
+        raise AirflowException('downstream_1 Exception!')
+    
+    
+    @task(task_id='python_upstream_2')
+    def python_upstream_2():
+        print('정상 처리')
+        
+        
+    @task(task_id='python_downstream_1', trigger_rule='all_done')
+    def python_downstream_1():
+        print('정상 처리')
+        
+    
+    [bash_upstream_1, python_upstream_1(), python_upstream_2()] >> python_downstream_1()
