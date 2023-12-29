4386285d72bf5b1fa6722a4adc91e97cf8824f20
YooSuYeon <usuyn000@gmail.com>
2023-12-29 21:16:22 +0900

    file sensor

--- dags/dags_file_sensor.py
+++ dags/dags_file_sensor.py
@@ -2,6 +2,7 @@ import pendulum
 
 from airflow import DAG
 from airflow.sensors.filesystem import FileSensor
+from airflow.operators.bash import BashOperator
 
 with DAG(
     dag_id='dags_file_sensor',
@@ -14,9 +15,14 @@ with DAG(
         fs_conn_id='conn_file_opt_airflow_files',
         filepath='tvCorona19VaccinestatNew/{{ data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/tvCorona19VaccinestatNew.csv',
         recursive=False,
-        poke_interval=60,
+        poke_interval=30,
         timeout=60 * 60 * 24, # 24시간
         mode='reschedule'
     )
+    
+    bash_task = BashOperator(
+        task_id='bash_task',
+        bash_command='echo file sensor success'
+    )
 
-    tvCorona19VaccinestatNew_sensor
+    tvCorona19VaccinestatNew_sensor >> bash_task
