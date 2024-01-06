40894134bf772b8a8ce1b4cc8267ddf18acd3ffe
YooSuYeon <usuyn000@gmail.com>
2024-01-06 23:40:27 +0900

    timeout 설정

---
+++ dags/dags_timeout_example_1.py
@@ -0,0 +1,34 @@
+import pendulum
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.bash import BashOperator
+from datetime import timedelta
+
+email_str = Variable.get('email_target')
+email_list = [email.strip() for email in email_str.split(',')]
+
+with DAG(
+    dag_id='dags_timeout_example_1',
+    start_date=pendulum.datetime(2023, 1, 6, tz='Asia/Seoul'),
+    catchup=False,
+    schedule=None,
+    dagrun_timeout=timedelta(minutes=1),
+    default_args={
+        'execution_timeout': timedelta(seconds=20),
+        'email_on_failure': True,
+        'email': email_list
+    }
+) as dag:
+    bash_sleep_30 = BashOperator(
+        task_id='bash_sleep_30',
+        bash_command='sleep 30',
+    )
+    
+    bash_sleep_10 = BashOperator(
+        trigger_rule='all_done',
+        task_id='bash_sleep_10',
+        bash_command='sleep 10',
+    )
+    
+    bash_sleep_30 >> bash_sleep_10
