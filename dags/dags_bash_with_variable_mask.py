eb39a6ab075f0cce4a9622b97f87e0ac3fa902a9
YooHaJin <yoohajin.dev@gmail.com>
2024-04-12 00:57:04 +0900

    dag id 변경

--- dags/dags_bash_with_variable_mask.py
+++ dags/dags_bash_with_variable_mask.py
@@ -4,7 +4,7 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    dag_id="dags_bash_with_variable",
+    dag_id="dags_bash_with_variable_mask",
     schedule="10 9 * * *",
     start_date=pendulum.datetime(2023, 12, 1, tz="Asia/Seoul"),
     catchup=False
